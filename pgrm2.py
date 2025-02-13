ad206605b34e1586eeb25a9e7b8078f5d3c9ffba
AshishKumarCS <kumarguptaashish00@gmail.com>
2025-02-13 13:01:02 +0530

    Pgrm2 addeed

---
+++ pgrm2.py
@@ -0,0 +1,15 @@
+'''
+Operators:
+	/ normal division
+	// floor division ==> return just the int (w/o rounding off) not the float
+	% modulo
+	** power
+'''
+
+a=11
+b=2
+
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
