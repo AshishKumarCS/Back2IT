3b2ea9dd58f12d2abb265141aeecefa9338a302d
AshishKumarCS <kumarguptaashish00@gmail.com>
2025-02-13 20:55:50 +0530

    Pgrm3 modified

--- pgrm3.py
+++ pgrm3.py
@@ -3,3 +3,12 @@ name = "Ashish"
 print("Hello, I am"+name)   # Hello, I amAshish
 
 print("Hello, I am",name)   # Hello, I am Ashish
+
+var = "Mango"
+
+print(var[0:-3])
+print(var[0:len(var)-3])  #both are same for every minus value in argument python take it is total length - provided value ==> here it is 5 - 3 = 2 ==> so will show 0,1 positioned values only
+
+print(var[-1:-3])   #5-1=4 to 5-3=2  --> Python can't backtrace like this
+
+print(var[-3:-1])   #5-3=2 to 5-1=4  --> 2,3 positioned value 
